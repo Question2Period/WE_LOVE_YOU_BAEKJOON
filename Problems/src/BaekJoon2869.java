a030b2fe0cb52500eec5a7b15bcbedbb86fd2ca4
SSAFYKING <chldnxo3@naver.com>
2022-01-28 20:41:26 +0900

    나눗셈 다시볼 문제

--- Problems/src/BaekJoon2869.java
+++ Problems/src/BaekJoon2869.java
@@ -1,31 +1,25 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 public class BaekJoon2869 {
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		System.setIn(new FileInputStream("C:\\ssafy\\BaekJoon\\Problems\\src\\input2869.txt"));
+//		System.setIn(new FileInputStream("C:\\ssafy\\BaekJoon\\Problems\\src\\input2869.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken()); //집합 A의 원소 개수
-		int b = Integer.parseInt(st.nextToken()); //집합 B의 원소 개수
-		long v = Long.parseLong(st.nextToken()); //집합 B의 원소 개수
-		long day=0;
-		if(a==v) {
-			System.out.println("1");
+		int a = Integer.parseInt(st.nextToken()); 
+		int b = Integer.parseInt(st.nextToken()); 
+		int v = Integer.parseInt(st.nextToken());
+		int c = v-a;
+		int d = a-b;
+		int day = 0;
+		day = c/d+1;
+		if(c%d!=0) {
+			day++;
 		}
-		for(int i=1;a>b;i++) {
-			day+=a;
-			if(day>=v) {
-				System.out.println(i);
-				break;
-			}
-			day-=b;
-		}
+		System.out.println(day);
 
 	}
 }
