5c59d1a95a0ba1da8b013ffa43fa426c81ec3884
SSAFYKING <chldnxo3@naver.com>
2022-03-06 19:34:05 +0900

    20220306

--- Problems/src/Silver/BaekJoon1654.java
+++ Problems/src/Silver/BaekJoon1654.java
@@ -2,35 +2,30 @@ package Silver;
 import java.io.*;
 import java.util.*;
 public class BaekJoon1654 {
-
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int length = 0;
-		int max = 0;
 		int k = Integer.parseInt(st.nextToken());
 		int n = Integer.parseInt(st.nextToken());
 		int[] arr = new int[k];
+		long max = 0;
 		for(int i = 0; i<k; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, arr[i]);
 		}
-		Arrays.sort(arr);
-		for(int i = arr[k-1]/n; i<arr[0];i++) {
-			for(int j = 0; j<k; j++) {
-				length+=(arr[j]/i);
+		max++;
+		long min = 0;
+		long lan = 0;
+		while(min<max) {
+			long sum=0;
+			lan= (max+min)/2;
+			for(int i = 0; i<k; i++) {
+				sum+=(arr[i]/lan);
 			}
-			if(length>=n) {
-				if(i>max) {
-					max=i;
-				}
-			}
-			else {
-				break;
-			}
-			length=0;
+			if(sum<n) max = lan;		
+			else  min = lan+1;
 		}
-		System.out.println(max);
+		System.out.println(min-1);
 	}
-
 }
---
+++ Theory/src/ball/hit4ball1.java
@@ -0,0 +1,10 @@
+package ball;
+
+public class hit4ball1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
